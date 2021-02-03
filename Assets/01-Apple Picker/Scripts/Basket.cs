10df5dd2b87fc44424a8ad469eef28077cbfc7b1
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-02-03 16:22:49 -0500

    Work Session 12

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,8 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;        // This line enables use of uGUI features.
 
 public class Basket : MonoBehaviour {
+    [Header("Set Dynamically")]
+    public Text scoreGT;
+
+    void Start() {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the Text Component of the GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
 
     void Update() {
         // Get the current screen position of the mouse from Input 
@@ -25,6 +37,18 @@ public class Basket : MonoBehaviour {
         GameObject collidedWith = coll.gameObject;
         if ( collidedWith.tag == "Apple" ) {
             Destroy(collidedWith);
+
+            // Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);
+            // Add points for catching the apple
+            score += 100;
+            //Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+
+            // Track the high score
+            if (score >HighScore.score) {
+                HighScore.score = score;
+            }
         }
     }          
 }
