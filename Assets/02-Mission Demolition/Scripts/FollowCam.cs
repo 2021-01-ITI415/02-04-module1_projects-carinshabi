bd268ce4071c29010105b6da3f2a2fef5cee7c38
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-02-07 17:19:36 -0500

    Mission Demolition Work Session 5

---
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+    static public GameObject POI; // The static point of interest 
+
+    [Header("Set Dynamically")]
+    public float camZ; // The desired Z pos of the camera 
+
+    void Awake() {
+        camZ = this.transform.position.z; 
+    }
+
+
+}
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
