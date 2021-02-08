7f84325557e17675a844feb6ffb75260b7456393
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-02-07 21:53:25 -0500

    Mission Demolition Work Session 9

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
     // fields set in the Unity Inspector pane
     [Header("Set in Inspector")]
     public GameObject          prefabProjectile;
@@ -16,7 +17,15 @@ public class Slingshot : MonoBehaviour {
 
     private Rigidbody projectileRigidbody; 
 
+    static public Vector3 LAUNCH_POS { 
+        get {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake() {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
