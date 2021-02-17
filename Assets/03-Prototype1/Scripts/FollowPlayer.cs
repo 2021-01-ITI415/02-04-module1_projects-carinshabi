c6cae02d1d081b1e484c0f46f779e392c34dd98b
Carin Shabi <crs255@scarletmail.rutgers.edu>
2021-02-16 20:08:37 -0500

    Work Session #1REAL

--- Assets/03-Prototype1/Scripts/FollowPlayer.cs
+++ Assets/03-Prototype1/Scripts/FollowPlayer.cs
@@ -1,39 +1,82 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class FollowPlayer : MonoBehaviour
-{
+public class FollowPlayer : MonoBehaviour {
+    static private FollowPlayer S;
+    // fields set in the Unity Inspector pane
     [Header("Set in Inspector")]
     public GameObject launchPoint;
     public GameObject playerBall;
+    public GameObject prefabApple;
+    public float velocityMult = 8f;
 
+    // fields set dynamically
     [Header("Set Dynamically")]
-
     public Vector3 launchPos;
+    public GameObject apple;
     public Transform launchPointTrans;
-    public Transform plyaerPointTrans;
+    public Transform playerPointTrans;
+    public bool aimingMode;
+    private Rigidbody playerRigidbody;
 
-    //Start is called before the first frame update
+    private Rigidbody appleRigidbody;
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
+    // Start is called before the first frame update
     void Start()
     {
 
     }
-    void Awake() {
-        // S = this; 
 
+    void Awake()
+    {
+        // S = this;
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
+    void OnMouseEnter()
+    {
+        print("FollowPlayer:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        print("FollowPlayer:OnMouseExit()");
         launchPoint.SetActive(false);
     }
-    //void OnMouseEnter() {
-    //   launchPoint.SetActive(true);
-    // }
-    // void OnMouseExit() {
-    //   launchPoint.SetActive(false);
-    // }
-    //Update is called once per frame
-    void Update() 
+
+    void OnMouseDown()
     {
-        Vector3 playerPos = playerBall.transform.position;
-        launchPoint.transform.position = playerPos;
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+        print("aiming mode true!");
+        //Start it at the launchPoint
+        playerBall.transform.position = launchPos;
+        playerBall.GetComponent<Rigidbody>().isKinematic = true;
+        playerRigidbody = playerBall.GetComponent<Rigidbody>();
+        playerRigidbody.isKinematic = true;
+        // Set it to isKinematic for now
+        appleRigidbody = apple.GetComponent<Rigidbody>();
+        appleRigidbody.isKinematic = true;
     }
+
+    void OnTriggerEnter(Collider other) { 
+        if (other.gameObject.CompareTag("Apple")) {
+            // Instantiate a Apple
+            Vector3 randomPos = new Vector3(Random.Range(-5.0f, 5.0f), 2.5f, Random.Range(-5.0f, 5.0f));
+            other.transform.position = randomPos;
+        }
+    } 
 }
---
+++ Assets/03-Prototype1/Scripts/SnakeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour { 
+    public Transform head;
+    public float moveSpeed, rotationSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        head.position += head.forward * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            head.Rotate(Vector3.up * -rotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            head.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+        }
+    }
+}
